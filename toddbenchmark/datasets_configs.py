c7a094ac7ad8ac33ab83f21f63525b3430f0bffd
Icannos <astaligorn@live.com>
2022-12-02 14:52:47 +0100

    add base config with batch_size and max_length for datasets configs

--- toddbenchmark/datasets_configs.py
+++ toddbenchmark/datasets_configs.py
@@ -2,32 +2,43 @@ from typing import Dict
 
 DATASETS_CONFIGS: Dict[Dict] = {}
 
+BASE_CONFIG = {
+    "batch_size": 8,
+    "max_length": 100,
+}
+
 # Datasets configs with english (en) as target language:
 
 # wmt16
-DATASETS_CONFIGS["wmt16_de_en"] = {"dataset_name": "wmt16", "dataset_config": "de-en"}
-DATASETS_CONFIGS["wmt16_fr_en"] = {"dataset_name": "wmt16", "dataset_config": "fr-en"}
+DATASETS_CONFIGS["wmt16_de_en"] = BASE_CONFIG | {
+    "dataset_name": "wmt16",
+    "dataset_config": "de-en",
+}
+DATASETS_CONFIGS["wmt16_fr_en"] = BASE_CONFIG | {
+    "dataset_name": "wmt16",
+    "dataset_config": "fr-en",
+}
 
 # news_commentary
-DATASETS_CONFIGS["news_commentary_en_de"] = {
+DATASETS_CONFIGS["news_commentary_en_de"] = BASE_CONFIG | {
     "dataset_name": "news_commentary",
     "dataset_config": "de-en",
 }
 
 # Helsinki-NLP TaToeba, german english
-DATASETS_CONFIGS["tatoeba_mt_deu_eng"] = {
+DATASETS_CONFIGS["tatoeba_mt_deu_eng"] = BASE_CONFIG | {
     "dataset_name": "Helsinki-NLP/tatoeba_mt",
     "dataset_config": "deu-eng",
 }
 
 # Helsinki-NLP TaToeba, Spanish English
-DATASETS_CONFIGS["tatoeba_mt_spa_eng"] = {
+DATASETS_CONFIGS["tatoeba_mt_spa_eng"] = BASE_CONFIG | {
     "dataset_name": "Helsinki-NLP/tatoeba_mt",
     "dataset_config": "spa-eng",
 }
 
 # Helsinki-NLP TaToeba, Catalan English
-DATASETS_CONFIGS["tatoeba_mt_cat_eng"] = {
+DATASETS_CONFIGS["tatoeba_mt_cat_eng"] = BASE_CONFIG | {
     "dataset_name": "Helsinki-NLP/tatoeba_mt",
     "dataset_config": "cat-eng",
 }
